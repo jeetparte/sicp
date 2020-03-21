96bca67b789afba7ebecc4ca39e842b6c9f28a96
Jeet Parte <jeetparte@gmail.com>
2020-03-22 01:32:29 +0530

    Update to include full solutions
    Add dependencies and testing scenarios.

--- chapter2/sicp2.2.rkt
+++ chapter2/sicp2.2.rkt
@@ -1,8 +1,5 @@
 #lang sicp
 
-(define (average a b)
-  (/ (+ a b) 2))
-
 ; Programs that use points and segments
 (define (midpoint-segment s)
   (let ((start (start-segment s))
@@ -11,6 +8,19 @@
                          (x-point end))
                 (average (y-point start)
                          (y-point end)))))
+
+; trying out nested `let` statements
+;(define (midpoint-segment s)
+;  (define (average a b)
+;    (/ (+ a b) 2))
+;  (let ((start (start-segment s))
+;        (end (end-segment s)))
+;    (let ((start-x (x-point start))
+;          (start-y (y-point start))
+;          (end-x (x-point end))
+;          (end-y (y-point end)))
+;      (make-point (average start-x end-x)
+;                  (average start-y end-y)))))
 
 ; Segments
 (define (make-segment start-point end-point)
@@ -27,3 +37,22 @@
   (car p))
 (define (y-point p)
   (cdr p))
+
+; Printing points
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+; Helpers
+(define (average a b)
+  (/ (+ a b) 2))
+
+; Testing
+(define point-a (make-point 0 0))
+(define point-b (make-point 4 10))
+(define line-segment (make-segment point-a point-b))
+(print-point (midpoint-segment line-segment))
