dbb8d90b6ffaf70232fc0adcd85fa367b6cd8e5f
Jeet Parte <jeetparte@gmail.com>
2020-03-25 23:07:13 +0530

    Update sicp2.6.rkt
    Touch up.

--- chapter2/sicp2.6.rkt
+++ chapter2/sicp2.6.rkt
@@ -6,15 +6,13 @@
   (lambda (f) (lambda (x) (f ((n f) x)))))
 
 ; Part 1
-
 ; Deriving 'one' from (add-1 zero):
 (add-1 zero) ; comments will go here
 (add-1 (lambda (f) (lambda (x) x))) ; evaluated zero
 (lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) x)) f) x)))) ; substituted parameter 'n' inside the body of add-1 with the procedural argument
 (lambda (f) (lambda (x) (f ((lambda (x) x) x)))) ; applied lambda procedure to 'f'
 (lambda (f) (lambda (x) (f x))) ; applied lambda procedure to 'x'
-
-(define one (lambda (f) (lambda (x) (f x))))
+(define one (lambda (f) (lambda (x) (f x)))) ; done!
 
 ; Similiarly deriving 'two' from (add-1 one), we find:
 (define two (lambda (f) (lambda (x) (f (f x)))))
