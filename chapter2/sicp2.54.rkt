eebf1fb91bf1f50f571dc5f65814febbfdc04925
Jeet Parte <jeetparte@icloud.com>
2021-05-17 18:19:06 +0530

    Correct solution

--- chapter2/sicp2.54.rkt
+++ chapter2/sicp2.54.rkt
@@ -2,6 +2,9 @@
 
 (define (equal? a b)
   (or
-   (eq? a b)
-   (and (equal? (car a) (car b))
+   (eq? a b) ; note - eq? handles empty lists correctly, i.e. (eq? '() '()) --> #t.
+             ; This is important when we recursively cdr down the argument lists and might call (equal? '() '()).
+   (and (pair? a)
+        (pair? b)
+        (equal? (car a) (car b))
         (equal? (cdr a) (cdr b)))))
